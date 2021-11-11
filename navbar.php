fdeb6a9a7a03818481560cd136a99fa23af7bda2
Heggel123 <80232532+Heggel123@users.noreply.github.com>
2021-11-11 08:31:56 +0700

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,77 @@
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand bg-white navbar-light border-bottom">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+      <div class="topnav">
+
+     <!-- <a class="nav-link" data-widget="pushmenu" href="#"><i class="fa fa-bars fa-lg" style="padding-left:260px"></i></a>  -->
+     <a class="nav-item" data-widget="pushmenu" href="#"><i class="fa fa-bars black-color fa-lg"></i></a>
+    <!-- <a class="nav-item d-none d-sm-inline-block" ></a> -->
+
+    <a href="#home">Home</a>
+    <a href="#news">News</a>
+    <a href="<?php echo site_url('admin/sosmed/') ?>">Contact</a>
+    <a href="<?php echo site_url('admin/biodata/') ?>">About</a>
+</div>
+</div>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block" >
+        <span class="nav-link"></span>
+      </li>
+    </ul>
+  </nav>
+  <!-- /.navbar -->
+  <!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+.topnav-centered a {
+  position:relative;
+}*/
+.black-color {
+  color:black;
+}
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.topnav {
+  overflow: hidden;
+  background-color: white;
+}
+
+.topnav a {
+  float: left;
+  color: black;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color:dodgerblue ;
+  color: white;
+}
+
+.topnav a.active {
+  background-color: ;
+  color: black;
+}
+</style>
+</head>
+
+<body>
+
+
+
+<!-- <div style="padding-left:16px"> -->
+  <!-- <h2>Top Navigation Example</h2>
+  <p>Some content..</p>
+</div> -->
+
+</body>
+</html>
